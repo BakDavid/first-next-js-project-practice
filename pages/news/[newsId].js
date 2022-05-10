b95817aff798bca8f8e77c078915f05a6658234d
Bak David <bak.david96@gmail.com>
2022-05-10 21:44:48 +0300

    Making dynamic pages and retrieving values from url

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/something-important
 export default function DetailPage() {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
   return <h1>DetailPage</h1>;
 }
